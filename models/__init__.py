1fdaf505406e5c5ca29e42033bb9e4396771b249
Abdellah <legion@DESKTOP-98S99PL.localdomain>
2025-07-14 04:38:33 -0700

    change init

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
-# models/__init__.py
 from . import company
 from . import employee
 from . import common_models
 from . import employee_related
 from . import regulatory_models
+from . import affiliation_type
+from . import affiliation
